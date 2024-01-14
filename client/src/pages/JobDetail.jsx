3a2f5ad450d79c6f751507260a283d78a9156d1b
Veasna Pen <Veasna3d@gmail.com>
2024-01-14 12:33:56 +0700

    Improve JobDetail Card

--- client/src/pages/JobDetail.jsx
+++ client/src/pages/JobDetail.jsx
@@ -20,7 +20,7 @@ const JobDetail = () => {
   return (
     <div className="container mx-auto">
       <div className="w-full flex flex-col md:flex-row gap-10">
-        {}
+        {/* Left Side */}
         <div className="w-full h-fit md:w-2/3 2xl:2/4 bg-white px-5 py-10 md:px-10 shadow-md">
           <div className="w-full flex items-center justify-between">
             <div className="w-3/4 flex gap-2">
@@ -55,7 +55,7 @@ const JobDetail = () => {
             <div className="bg-[#bdf4c8] w-40 h-16 rounded-lg flex flex-col items-center justify-center">
               <span className="text-sm">Salary</span>
               <p className="text-lg font-semibold text-gray-700">
-                {job?.salary}
+                $ {job?.salary}
               </p>
             </div>
 
@@ -66,8 +66,8 @@ const JobDetail = () => {
               </p>
             </div>
 
-            <div className="bg-[#fed0ab] w-40 h-16 px-6 rounded-lg flex flex-col items-center justify-center">
-              <span className="text-sm">No. fo Application</span>
+            <div className="bg-[#fed0ab] w-41 h-16 px-6 rounded-lg flex flex-col items-center justify-center">
+              <span className="text-sm">No. of Applications</span>
               <p className="text-lg font-semibold  text-gray-700">
                 {job?.applicants?.length}
               </p>
@@ -80,10 +80,76 @@ const JobDetail = () => {
               </p>
             </div>
           </div>
-        </div>
 
-        {}
-        <div></div>
+          <div className="w-full flex gap-4 py-5">
+            <CustomButton
+              onClick={() => setSelected("0")}
+              title="Job Description"
+              containerStyles={`w-full flex items-center justify-center py-3 px-5 outline-none  rounded-full text-sm ${
+                selected === "0"
+                  ? "bg-black text-white"
+                  : "bg-white text-black border border-gray-300"
+              }`}
+            />
+
+            <CustomButton
+              onClick={() => setSelected("1")}
+              title="Company"
+              containerStyles={`w-full flex items-center justify-center py-3 px-5 outline-none  rounded-full text-sm ${
+                selected === "1"
+                  ? "bg-black text-white"
+                  : "bg-white text-black border border-gray-300"
+              }`}
+            />
+          </div>
+
+          <div className="my-6">
+            {selected === "0" ? (
+              <>
+                <p className="text-xl font-semibold">Job Description</p>
+                <span className="text-base">{job?.detail[0]?.desc}</span>
+
+                {job?.detail[0]?.requirement && (
+                  <>
+                    <p className="text-xl font-semibold mt-8">Requirement</p>
+                    <span className="text-base">
+                      {job?.detail[0]?.requirement}
+                    </span>
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                <div className="mb-6 flex flex-col">
+                  <p className="text-lg text-blue-600 font-semibold">
+                    {job?.company?.name}
+                  </p>
+                  <p className="text-base">{job?.company?.location}</p>
+                  <p className="text-sm">{job?.company?.email}</p>
+                </div>
+
+                <p className="text-xl font-semibold">About Company</p>
+                <span>{job?.company?.about}</span>
+              </>
+            )}
+          </div>
+
+          <div className="w-full">
+            <CustomButton
+              title="Apply Now"
+              containerStyles={`w-full flex items-center justify-center text-white bg-black py-3 px-5 outline-none rounded-full text-base`}
+            />
+          </div>
+        </div>
+        {/* Right Side */}
+        <div className="w-full md:w-1/3 2xl:w-2/4 p-5 mt-20 md:mt-0">
+          <p className="text-gray-500 font-semibold">Similar Job Post</p>
+          <div className="w-full flex flex-wrap gap-4">
+            {jobs?.slice(0, 6).map((job, index) => (
+              <JobCard job={job} key={index} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
